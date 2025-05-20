7a98269f2a416e00f9063a1191738e7289dafad8
adri4nlm <adrianlm@n2x.io>
2025-05-20 13:48:43 +0200

    feat: add ansible roles docker and wireguard

--- exec-infra.sh
+++ exec-infra.sh
@@ -1,28 +1,65 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 #tmpfile=$(mktemp)
 tmpfile="$(pwd)/.temp.infra"
 date=$(date "+%Y-%m-%d.%H-%M")
 
-touch $tmpfile && printf "Create temp file: $tmpfile\n"
-
-docker build --no-cache -t wireguard-container docker/images/wireguard
+touch $tmpfile && printf "Temp file: $tmpfile\n"
 
 destroy_docker_images(){
   docker stop $@
   docker rm -f $@
 }
 
-[ $1 == "destroy" ] && {
-  mapfile -t DOCKER_CONTAINERS < <(grep "docker:" .temp.infra |  cut -d':' -f2) # awk -F ':' '{printf $2}'
-  destroy_docker_images ${DOCKER_CONTAINERS[@]}
+destroy_multipass_vms(){
+  sudo multipass delete $@ || return 1
+  sudo multipass purge
 }
 
-[ $1 == "wg-test" ] && {
-  printf '\ndocker:' >> $tmpfile
-  docker run --name wireguard.$date \
+
+destroy(){
+  mapfile -t DOCKER_CONTAINERS < <(grep "docker:" .temp.infra |  cut -d':' -f2) # awk -F ':' '{printf $2}'
+  for i in ${DOCKER_CONTAINERS[@]}; do
+    destroy_docker_images $i || { printf "[!] Error destroy container ($i)"; exit 1; }
+  done
+   
+
+  mapfile -t VMS_LIST < <(grep "multipass:" .temp.infra |  cut -d':' -f2)
+  for i in ${VMS_LIST[@]}; do
+    destroy_multipass_vms $i || { printf "[!] Error destroy vm ($i)"; exit 1; }
+  done
+}
+
+build(){
+  printf "[+] Deploy infrastructure\n"
+  ANSIBLE="ansible"
+  HUB_HOST="server01"
+  PEERS_HOST="node0"
+  PEERS_NUM=2
+
+  printf "[+] Create ansible node\n"
+  printf '\ndocker:' >> $tmpfile; docker run -d \
+  --name $ANSIBLE \
+  --network host \
+  -v "$(pwd)/docker/containers/ansible/ansible:/ansible" \
+  -v "$(pwd)/docker/containers/ansible/ssh:/root/.ssh/" \
+  -w /ansible \
+  willhallonline/ansible:latest \
+  sleep infinity 1>> $tmpfile 2>/dev/tty
+
+  printf '\t[+] Create VM (HUB)'
+  ./scripts/multipass-setup.sh single $HUB_HOST "4G" "20G" 2
+
+  printf '\t[+] Create VMs (peers)'
+  ./scripts/multipass-setup.sh multi $PEERS_HOST "4G" "20G" 2 3
+}
+
+wg-test(){
+  docker build --no-cache -t wireguard-container docker/images/wireguard
+
+  printf 'docker:' >> $tmpfile; docker run --name wireguard.$date \
     --cap-add=NET_ADMIN \
     --cap-add=SYS_MODULE \
     --net=host \
@@ -31,3 +68,5 @@ destroy_docker_images(){
     -d wireguard-container 1>> $tmpfile 2>/dev/tty
 }
 
+
+[[ $1 =~ (build|destroy) ]] && { eval $1; }
--- scripts/multipass-setup.sh
+++ scripts/multipass-setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # This script sets up a multipass instance with the necessary configurations for monitoring.
 
+# Global
+tmpfile="$(pwd)/.temp.infra"
+SSH_KEY_PATH="docker/containers/ansible/ssh/id_rsa_ansible.pem.pub"
+SSH_PRIVATE_KEY_PATH="docker/containers/ansible/ssh/id_rsa_ansible.pem"
+
 # Check if multipass is installed
 if ! command -v multipass &> /dev/null
 then
@@ -8,53 +13,34 @@ then
     exit 1
 fi
 
-# Global
-SSH_KEY_PATH="docker/containers/ansible/ssh/id_rsa_ansible.pem.pub"
-SSH_PRIVATE_KEY_PATH="docker/containers/ansible/ssh/id_rsa_ansible.pem"
+deploy_vm(){
+    SERVER_NAME=$1
+    SERVER_MEMORY=$2
+    SERVER_DISK=$3
+    SERVER_CPUS=$4
 
-# Create a server instance
-SERVER_NAME="monitoring-server"
-SERVER_MEMORY="4G"
-SERVER_DISK="20G"
-SERVER_CPUS=2
+    # Launch the server instance
+    sudo multipass launch --name "$SERVER_NAME" --cpus "$SERVER_CPUS" --memory "$SERVER_MEMORY" --disk "$SERVER_DISK" && {
+        printf "\nmultipass:$SERVER_NAME" >> $tmpfile
+    }
 
-# Launch the server instance
-sudo multipass launch --name "$SERVER_NAME" --cpus "$SERVER_CPUS" --memory "$SERVER_MEMORY" --disk "$SERVER_DISK"
+    # Install and enable SSH server
+    sudo multipass exec "$SERVER_NAME" -- sudo apt-get update
+    sudo multipass exec "$SERVER_NAME" -- sudo apt-get install -y openssh-server
+    sudo multipass exec "$SERVER_NAME" -- sudo bash -c "echo $(cat $SSH_KEY_PATH) >> /home/ubuntu/.ssh/authorized_keys"
+    sudo multipass exec "$SERVER_NAME" -- sudo systemctl enable ssh
+    sudo multipass exec "$SERVER_NAME" -- sudo systemctl start ssh
 
-# Install and enable SSH server
-sudo multipass exec "$SERVER_NAME" -- sudo apt-get update
-sudo multipass exec "$SERVER_NAME" -- sudo apt-get install -y openssh-server
-sudo multipass exec "$SERVER_NAME" -- sudo bash -c "echo $(cat $SSH_KEY_PATH) >> /home/ubuntu/.ssh/authorized_keys"
-sudo multipass exec "$SERVER_NAME" -- sudo systemctl enable ssh
-sudo multipass exec "$SERVER_NAME" -- sudo systemctl start ssh
-
-# Display server instance information
-echo "You can now SSH into the server instance using the following command:"
-IP_ADDRESS=$(sudo multipass info "$SERVER_NAME" | grep "IPv4" | awk '{print $2}')
-echo "ssh -i $SSH_PRIVATE_KEY_PATH ubuntu@$IP_ADDRESS"
-
-# Create a multipass instance
-NUM_INSTANCES=3
-INSTANCE_NAME="node"
-
-for i in $(seq 1 $NUM_INSTANCES); do
-    sudo multipass launch --name "$INSTANCE_NAME-$i" --cpus 1 --memory 2G --disk 20G
-done
-
-# Install and enable SSH server
-for i in $(seq 1 $NUM_INSTANCES); do
-    sudo multipass exec "$INSTANCE_NAME-$i" -- sudo apt-get update
-    sudo multipass exec "$INSTANCE_NAME-$i" -- sudo apt-get install -y openssh-server
-    sudo multipass exec "$INSTANCE_NAME-$i" -- sudo bash -c "echo $(cat $SSH_KEY_PATH) >> /home/ubuntu/.ssh/authorized_keys"
-    sudo multipass exec "$INSTANCE_NAME-$i" -- sudo systemctl enable ssh
-    sudo multipass exec "$INSTANCE_NAME-$i" -- sudo systemctl start ssh
-done
-
-echo "Multipass setup completed successfully."
-
-# Display connection information
-echo "You can now SSH into the instances using the following command:"
-for i in $(seq 1 $NUM_INSTANCES); do
-    IP_ADDRESS=$(sudo multipass info "$INSTANCE_NAME-$i" | grep "IPv4" | awk '{print $2}')
+    # Display server instance information
+    echo "You can now SSH into the server instance using the following command:"
+    IP_ADDRESS=$(sudo multipass info "$SERVER_NAME" | grep "IPv4" | awk '{print $2}')
     echo "ssh -i $SSH_PRIVATE_KEY_PATH ubuntu@$IP_ADDRESS"
-done
+}
+
+[ $1 == "single" ] && { deploy_vm $2 $3 $4 $5; }
+
+[ $1 == "multi" ] && {
+    for i in $(seq 1 $6); do
+        deploy_vm "$2$i" $3 $4 $5
+    done
+}
